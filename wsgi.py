5d7361b4efa6ff9a0c1b105ce9f183fd72cd8f97
Sun Yang <ysunbj@yahoo.com>
2011-05-08 19:20:28 +0800

    fix indentation

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ import django.core.handlers.wsgi
 djangoapplication = django.core.handlers.wsgi.WSGIHandler()
 def application(environ, start_response):
     if 'SCRIPT_NAME' in environ:
-            del environ['SCRIPT_NAME']
-                return djangoapplication(environ, start_response)
+        del environ['SCRIPT_NAME']
+        return djangoapplication(environ, start_response)
