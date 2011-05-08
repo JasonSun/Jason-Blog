7cf167b60dd1140e688a754b8da207bac8fcbe0b
Sun Yang <ysunbj@yahoo.com>
2011-05-08 23:34:26 +0800

    update admin media dir

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ MEDIA_URL = 'http://127.0.0.1:8000/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/home/dotcloud/env/lib/python2.6/site-packages/django/contrib/admin/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin_media/'
 
 TEMPLATE_DEBUG = DEBUG
 
