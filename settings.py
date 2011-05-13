da9a62064065e54f83d353c106cab67472bba356
Sun Yang <ysunbj@yahoo.com>
2011-05-13 10:09:24 +0800

    update TIME-ZONE setting

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ TEMPLATE_DEBUG = DEBUG
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
